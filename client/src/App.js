8c78f21423f0c37f330c3bf4a93af480dbcb8a1a
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-07-01 17:43:00 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Login from './Login';
+import CompanyAnnouncements from './pages/CompanyAnnouncements';
+import EmployeeBenefitsDashboard from './pages/EmployeeBenefitsDashboard';
+import UnauthorizedAccess from './pages/UnauthorizedAccess';
+import AuthRouteGuard from './components/AuthRouteGuard';
 
-function App() {
-  return (
- <Login/>
-  );
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/company-announcements" element={<CompanyAnnouncements />} />
+      <Route
+        path="/employee-benefits"
+        element={
+          <AuthRouteGuard>
+            <EmployeeBenefitsDashboard />
+          </AuthRouteGuard>
+        }
+      />
+      <Route path="/unauthorized-access" element={<UnauthorizedAccess />} />
+    </Routes>
+  </>
+);
 
 export default App;
