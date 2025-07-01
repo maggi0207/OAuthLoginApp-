c50ebae013e9e7e54fe734589ec0a09811301b7b
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-07-01 17:42:07 +0530

    Create UnauthorizedAccess.js

---
+++ client/src/pages/UnauthorizedAccess.js
@@ -0,0 +1,8 @@
+const UnauthorizedAccess = () => (
+  <div style={{ padding: 20 }}>
+    <h2>🚫 Access Denied</h2>
+    <p>You must be logged in to access this page.</p>
+  </div>
+);
+
+export default UnauthorizedAccess;
