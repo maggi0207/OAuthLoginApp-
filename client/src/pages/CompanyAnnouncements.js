a8c4b7582f8792692e83d1b2293e0e65d7e05b92
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-07-01 17:40:47 +0530

    Create CompanyAnnouncements.js

---
+++ client/src/pages/CompanyAnnouncements.js
@@ -0,0 +1,12 @@
+const CompanyAnnouncements = () => (
+  <div style={{ padding: 20 }}>
+    <h2>📣 Company Announcements</h2>
+    <ul>
+      <li>🗓️ Townhall meeting on July 10th</li>
+      <li>🎉 Diwali Bonus to be released next month</li>
+      <li>📢 New office opening in Bangalore</li>
+    </ul>
+  </div>
+);
+
+export default CompanyAnnouncements;
